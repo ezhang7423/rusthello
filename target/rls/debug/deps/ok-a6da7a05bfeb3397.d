9332e828b45c2161d7d4af8a6289cb16419d7351
ezhang7423 <ezhang7423@student.palomar.edu>
2020-05-14 20:59:48 -0700

    ok

---
+++ target/rls/debug/deps/ok-a6da7a05bfeb3397.d
@@ -0,0 +1,5 @@
+/home/ete/github/rusthello/target/rls/debug/deps/ok-a6da7a05bfeb3397.rmeta: src/main.rs
+
+/home/ete/github/rusthello/target/rls/debug/deps/ok-a6da7a05bfeb3397.d: src/main.rs
+
+src/main.rs:
